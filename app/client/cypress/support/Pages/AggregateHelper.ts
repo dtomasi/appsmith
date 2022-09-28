7308c5013c316bbb7fabcef1976d2bd066aa691f
Aishwarya UR <aishwarya@appsmith.com>
2022-09-28 19:04:30 +0530

    PostMessage spec fix

--- app/client/cypress/support/Pages/AggregateHelper.ts
+++ app/client/cypress/support/Pages/AggregateHelper.ts
@@ -392,6 +392,10 @@ export class AggregateHelper {
     cy.get("body").type("{esc}");
   }
 
+  public PressEnter() {
+    cy.get("body").type("{enter}");
+  }
+
   public PressDelete() {
     cy.get("body").type(`{del}`, { force: true });
   }
